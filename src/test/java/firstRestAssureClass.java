7914d6c574c9c93ecd2b23e600f0d2115f32acf9
pahariyatri <pahariyatri@gmail.com>
2022-05-03 16:58:05 +0530

    first commit

---
+++ src/test/java/firstRestAssureClass.java
@@ -0,0 +1,21 @@
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.Test;
+
+public class firstRestAssureClass {
+    @Test
+    public void get(){
+        // Specify the base URL to the RESTfull web service
+        RestAssured.baseURI = "https://demoqa.com/BookStore/v1/Books";
+        // Get the RequestSpecification of the request to be sent to the server.
+        RequestSpecification httpRequest = RestAssured.given();
+        // specify the method type (GET) and the parameters if any.
+        //In this case the request does not take any parameters
+        Response response = httpRequest.request(Method.GET, "");
+        // Print the status and message body of the response received from the server
+        System.out.println("Status received => " + response.getStatusLine());
+        System.out.println("Response=>" + response.prettyPrint());
+    }
+}
